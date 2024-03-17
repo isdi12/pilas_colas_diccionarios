01b083a3a6fa0a8b97145f0beca5f2d9068fc78d
isdi12 <ismaelsesma11@gmail.com>
2024-03-17 23:41:12 +0100

    diccionario medio hecho
    diccionario medio hecho

--- estructura de datos/Assets/script/Diccionario.cs
+++ estructura de datos/Assets/script/Diccionario.cs
@@ -7,7 +7,10 @@ public class Diccionario : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Dictionary<char, string> diccionario = new Dictionary<char, string>();
+        diccionario.Add('€' , 'Euro');
+        diccionario.Add('$' , 'Dollar'):
+        diccionario.Add('¥', 'Yen');
     }
 
     // Update is called once per frame
